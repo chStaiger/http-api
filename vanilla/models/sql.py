a8cb6125075ac9748c7108de06abdd3af5974465
muccix <muccix@gmail.com>
2017-03-14 13:04:22 +0100

    Fixed setattr name and surname

--- vanilla/models/sql.py
+++ vanilla/models/sql.py
@@ -9,8 +9,8 @@ from ..sql import db, User, ExternalAccounts
 # logger = logging.get_logger(__name__)
 
 # Add (inject) attributes to User
-setattr(User, 'name', db.Column(db.String(255)))
-setattr(User, 'surname', db.Column(db.String(255)))
+#setattr(User, 'name', db.Column(db.String(255)))
+#setattr(User, 'surname', db.Column(db.String(255)))
 
 # Let iRODS exist and be linked
 setattr(ExternalAccounts, 'irodsuser', db.Column(db.String(50)))
