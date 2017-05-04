7f4e33ffb20b9b0c36b64e0bd0d7ada0e24c3097
Massimo <massimo.fares@ingv.it>
2017-05-04 12:11:01 +0200

    continue to build the API for EPOS, Mongo access and query ok POC

--- vanilla/apis/airods.py
+++ vanilla/apis/airods.py
@@ -4,6 +4,7 @@
 
 from ..rest.definition import EndpointResource
 from commons.logs import get_logger
+import dateutil.parser
 import json
 
 log = get_logger(__name__)
@@ -12,19 +13,48 @@ RESPONSE1 = [{'pid' : '11099/qwoprerc-343536ssdfjs-34500-asdpwe94383' ,'network'
             
 RESPONSE2 = [{"pid" : "11099/qwoprerc-343536ssdfjs-34500-asdpwe94383" ,"metadata": {"dc:contributor" : "network operator","dcterms:dateAccepted" : "2017-03-06T11:35:07.114Z","dc:identifier" : "test/f2c3ea40-0260-11e7-9f93-0242ac110008","dc:type" : "seismic waveform","dc:subject" : "mSEED, waveform, quality","dcterms:isPartOf" : "wfmetadata_catalog","dc:title" : "INGV_Repository","dc:rights" : "open access","dc:format" : "MSEED","dcterms:available" : "available from now","dc:date" : "2017-03-06T11:35:07.114Z","dc:coverage:x" : "LAT_val","dc:publisher" : "INGV EIDA NODE","dc:creator" : "INGV EIDA NODE","dc:coverage:t:min" : "time_start_val","dc:coverage:t:max" : "time_end_val","dc:coverage:x" : "LAT_val","dc:coverage:y" : "LON_val","dc:coverage:z" : "ELE_val","fileId" : "IV.ARCI..HHN.D.2015.011","smean" : 587.6017203352374,"stdev" : 25154.453024673716,"rms" : 25161.315203149203,"fileId" : "IV.ARCI..HHN.D.2015.011","type" : "seismic","status" : "open","glen" : 27054.59,"enc" : "STEIM2","srate" : 100,"gmax" : 27054.59,"sta" : "ARCI","net" : "IV","cha" : "HHN","loc" : ""}}]
 
+
+
 class TestMongo(EndpointResource):
 
     def get(self):
         log.info("just a test")
-        mongohd = self.global_get_service('mongo', dbname='mytest')
-        
-        # --> important into mongo collections we must have: 
-        #     "_cls" : "commons.models.custom.mongo.Testing"
-        mongohd.Testing(onefield='justatest').save()
-        # log.pp(mongohd)
-        print(mongohd)
+        mongohd = self.global_get_service('mongo', dbname='test')
         myargs= self.get_input()
         print(myargs)
+       
+        documentResult1 = []
+        # --> important into mongo collections we must have: 
+        #     "_cls" : "commons.models.custom.mongo.Testing"
+        
+        #mongohd.wf_do(fileId='justatest').save()
+        #BlogPost.objects.raw({'tags': 'slideshow'})
+        #"dc_coverage_t_min": "2015-01-11T00:00:00Z" "IV.ARCI..HHN.D.2015.011" , "dc_coverage_x": { "$lte" : myfloat } ,"dc_coverage_t_min": { "$gte" : myStartDate },"dc_coverage_t_max": { "$lte" : myEndDate }
+        
+        mycollection = mongohd.wf_do
+        # log.pp(mongohd)
+        myStartDate = dateutil.parser.parse(myargs.get("start"))
+        myEndDate = dateutil.parser.parse(myargs.get("end"))
+        myLat = float(myargs.get("minlat"))
+        myLon = float(myargs.get("minlon"))
+        myLatX = float(myargs.get("maxlat"))
+        myLonX = float(myargs.get("maxlon"))
+        
+        myfirstvalue = mycollection.objects.raw({"dc_coverage_x": { "$gte" : myLat }, "dc_coverage_y": { "$gte" : myLon }, "dc_coverage_x": { "$lte" : myLatX }, "dc_coverage_y": { "$lte" : myLonX },"dc_coverage_t_min": { "$gte" : myStartDate },"dc_coverage_t_max": { "$lte" : myEndDate }})
+        
+        #myfirstvalue = mongohd.wf_do.objects.all()
+        
+        #print(myfirstvalue.count())
+        for document in myfirstvalue:
+            documentResult1.append(document.fileId)
+            #documentResult.append(document)
+            
+            #arrayResult[document.fileId] = documentResult
+            
+        
+        #documentResult1.append(documentResult)  
+        #print(documentResult1)
+        
         
         if myargs.get('download') == 'true':
             # TO FIX: problem with swagger-ui boolean?
@@ -33,7 +63,8 @@ class TestMongo(EndpointResource):
         else:
             #n = json.dumps(RESPONSE1)  
             #o = json.loads(n)
-            return  RESPONSE1
+            return  ["total files:"+str(len(documentResult1)),documentResult1] 
+        
         
         #"it works! - no download" str.replace(RESPONSE1, '\n', '\r\n') TestMongoMeta
 
--- vanilla/tests/test_airods.py
+++ vanilla/tests/test_airods.py
@@ -15,7 +15,10 @@ log = get_logger(__name__)
 
 class TestSomething(RestTestsAuthenticatedBase):
 
-    _main_endpoint = '/airodsmongo'
+    _main_endpoint = '/statics/metadata' # qui l'endpoint dichiarati in specs.yaml /statics/metadata
+    _my_params = '?start="12-02-2017T00.00.00"&end="13-02-2017T00.00.00"&minlat=12.30&minlon=12.30&maxlat=12.30&maxlon=12.30&download=False'
+    #_main_endpoint = '/airodsmongo' # qui l'endpoint dichiarati in specs.yaml /statics/metadata
+   
     # _irods_test_name = 'test'
     # _irods_home = '/tempZone/home/guest'
     # _irods_path = '/tempZone/home/guest/test'
@@ -37,6 +40,10 @@ class TestSomething(RestTestsAuthenticatedBase):
         """ First test """
 
         log.info('*** Testing GET')
-        endpoint = (self._api_uri + self._main_endpoint)
+        endpoint = (self._api_uri + self._main_endpoint + self._my_params)
         r = self.app.get(endpoint)  # , headers=self.__class__.auth_header)
+        print ("QUESTO!")
+        print (r.data)
         self.assertEqual(r.status_code, self._hcodes.HTTP_OK_BASIC)
+        log.info('*** Testing GET FAKE - OK')
+        
